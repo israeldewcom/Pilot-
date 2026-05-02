2a5b0eb4b22af1d5920eb662470325d26addfb31
israeldewcom <haddockthor@gmail.com>
2026-05-02 09:57:26 +0100

    Add decorators for public access and user roles

---
+++ src/common/decorators/decorators.ts
@@ -0,0 +1,29 @@
+import { createParamDecorator, ExecutionContext, SetMetadata } from '@nestjs/common';
+
+export const IS_PUBLIC_KEY = 'isPublic';
+export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
+
+export const ROLES_KEY = 'roles';
+export const Roles = (...roles: string[]) => SetMetadata(ROLES_KEY, roles);
+
+export interface RequestUser {
+  id: string;
+  externalId: string;
+  organizationId: string;
+  email: string;
+  role: string;
+}
+
+export const CurrentUser = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext): RequestUser => {
+    const request = ctx.switchToHttp().getRequest();
+    return request.user;
+  },
+);
+
+export const CurrentTenant = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    return request.tenant;
+  },
+);
